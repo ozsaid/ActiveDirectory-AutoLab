9e9e269f9861344b836264906de30841f6018d2f
Oz Said <61497479+ozsaid@users.noreply.github.com>
2021-07-01 15:57:30 +0300

    Add files via upload

---
+++ Labs/Answers/UsersCsv.ps1
@@ -0,0 +1,6 @@
+﻿New-ADOrganizationalUnit Sales
+New-ADOrganizationalUnit Marketing
+New-ADOrganizationalUnit Research
+New-ADOrganizationalUnit Finance
+
+Import-Csv .\LIST.csv | % {New-ADUser -Name $_.DISP -GivenName $_.GivenName -Surname $_.Surename -City $_.City -SamAccountName $_.SAM -UserPrincipalName $_.UPN -Department $_.DEP -Description $_.DES -DisplayName $_.DISP -Path $_.PATH -Company $_.COMP -AccountPassword (ConvertTo-SecureString $_.PASS -AsPlainText -Force) -Enabled $true -PasswordNeverExpires $true -PassThru}
