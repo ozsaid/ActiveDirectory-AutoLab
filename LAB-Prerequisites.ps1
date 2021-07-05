68a8ef7cc0cc2cfcdc55baf183ae8deaf48380f6
Oz Said <61497479+ozsaid@users.noreply.github.com>
2021-07-05 12:44:52 +0300

    Update LAB-Prerequisites.ps1

--- LAB-Prerequisites.ps1
+++ LAB-Prerequisites.ps1
@@ -1,3 +1,5 @@
 ﻿Set-VMHost -VirtualHardDiskPath C:\VHD -VirtualMachinePath C:\VHD
 if ((Get-VMSwitch -Name Pri-TA).SwitchType -eq 'Private') {Remove-VMSwitch -Name Pri-TA -Force}
 if ((Get-VMSwitch -Name Pri-NY).SwitchType -eq 'Private') {Remove-VMSwitch -Name Pri-NY -Force}
+mkdir C:\VHD
+mkdir C:\BASE
