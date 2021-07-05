e7652aea8804d53185ba06253698a5528004d91f
Oz Said <61497479+ozsaid@users.noreply.github.com>
2021-07-05 12:41:40 +0300

    Add files via upload
    this file should be running before the lab creation
    
    it will change hyper-v default location to C:\VHD 
    and will erase older virtual switches created by this lab if any...

---
+++ LAB-Prerequisites.ps1
@@ -0,0 +1,3 @@
+﻿Set-VMHost -VirtualHardDiskPath C:\VHD -VirtualMachinePath C:\VHD
+if ((Get-VMSwitch -Name Pri-TA).SwitchType -eq 'Private') {Remove-VMSwitch -Name Pri-TA -Force}
+if ((Get-VMSwitch -Name Pri-NY).SwitchType -eq 'Private') {Remove-VMSwitch -Name Pri-NY -Force}
